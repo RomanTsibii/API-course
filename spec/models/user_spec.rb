224e73260a1e69cb5a72755f7ad4ef88d442f407
RomanTsibii <romanot70@gmail.com>
2020-03-04 13:14:27 +0200

    create model user, 2 validates + rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#validation' do
+    it 'should have valid factory' do
+      user = build :user
+      expect(user).to be_valid
+    end
+
+    it 'should validation presence of attributes' do
+      user = build :user, login: nil, provider: nil
+
+      expect(user).not_to be_valid
+      expect(user.errors.messages[:login]).to include("can't be blank")
+      expect(user.errors.messages[:provider]).to include("can't be blank")
+    end
+
+    it 'should validate uniqueness of login' do
+      user = create :user
+      other_user = build :user, login: user.login
+      expect(other_user).not_to be_valid
+      other_user.login = 'new user'
+      expect(other_user).to be_valid
+    end
+  end
+end
